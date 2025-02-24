3ccec3cb6bd59a1ab5d6228d6edb07c121d0a7aa
Vanshaj Kataria <90422718+vanshajkataria@users.noreply.github.com>
2025-02-24 14:22:53 +0530

    Added functionality to see the chosen choices

--- script.js
+++ script.js
@@ -9,25 +9,33 @@ const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 
-const options = ["rock", "paper", "scissors"];
+const options = ["Rock", "Paper", "Scissors"];
 const resultText = document.getElementById("result-text");
+const personChose = document.getElementById("person-chose");
+const computerChose = document.getElementById("computer-chose");
 
 const game = (choice) => {
     const computerChoice = Math.floor(Math.random()*3);
     console.log(computerChoice)
     console.log(`Person -> ${choice}`);
     console.log(`Computer -> ${options[computerChoice]}`)
-    if(choice==="rock" && options[computerChoice]==="scissors" || choice==="paper" && options[computerChoice]==="rock" || choice==="scissors" && options[computerChoice]==="paper"){
+    if(choice==="Rock" && options[computerChoice]==="Scissors" || choice==="Paper" && options[computerChoice]==="Rock" || choice==="Scissors" && options[computerChoice]==="Paper"){
         person.innerText = personScore+1;
         personScore++;
+        personChose.innerText = `${choice}`;
+        computerChose.innerText = `${options[computerChoice]}`;
         resultText.innerHTML = `<span>Player Wins</span>`;
     }
     else if(choice===options[computerChoice]){
+        personChose.innerText = `${choice}`;
+        computerChose.innerText = `${options[computerChoice]}`;
         resultText.innerHTML = `<span>It's a Tie</span>`;
     }
     else{
         computer.innerText = computerScore+1;
         computerScore++;
+        personChose.innerText = `${choice}`;
+        computerChose.innerText = `${options[computerChoice]}`;
         resultText.innerHTML = `<span>Computer Wins</span>`;
     }
 };
@@ -37,11 +45,13 @@ const reset = () => {
     computerScore = 0;
     person.innerText = personScore;
     computer.innerText = computerScore;
+    personChose.innerText = "";
+    computerChose.innerText = "";
     resultText.innerHTML = "";
 }
 
-rock.addEventListener("click", ()=>{game("rock")})
-paper.addEventListener("click", ()=>{game("paper")})
-scissors.addEventListener("click", ()=>{game("scissors")})
+rock.addEventListener("click", ()=>{game("Rock")})
+paper.addEventListener("click", ()=>{game("Paper")})
+scissors.addEventListener("click", ()=>{game("Scissors")})
 
 resetBtn.addEventListener("click", ()=>{reset()})
